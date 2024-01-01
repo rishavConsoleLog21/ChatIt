304daf392bb68ce427ea5e763562420bccdb3c48
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-01-01 15:44:58 +0530

    +Auth UI for Login & Register

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -2,7 +2,12 @@
 
 import { useCallback, useState } from "react";
 import { FieldValues, useForm, SubmitHandler } from "react-hook-form";
+import { BsGithub, BsGoogle } from "react-icons/bs";
+
 import Input from "@/app/components/input/Input";
+import Button from "@/app/components/Button";
+import AuthSocialButton from "./AuthSocialButton";
+
 type Variant = "LOGIN" | "REGISTER";
 
 const AuthForm = () => {
@@ -59,8 +64,71 @@ const AuthForm = () => {
         "
       >
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-          <Input id="email" label="Email" register={register} errors={errors}/>
+          {variant === "REGISTER" && (
+            <Input
+              id="name"
+              label="Name"
+              register={register}
+              errors={errors}
+              disabled={isLoading}
+            />
+          )}
+          <Input
+            id="email"
+            label="Email address"
+            type="email"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === "LOGIN" ? "Sign in" : "Register"}
+            </Button>
+          </div>
         </form>
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+
+          <div className="nt-6 flex gap-2">
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => {
+                socialAction("github");
+              }}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => {
+                socialAction("google");
+              }}
+            />
+          </div>
+        </div>
+
+        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+          <div>{variant === "LOGIN" ? "New To ChatIt" : "Have Account?"}</div>
+          <div onClick={toggleVariant} className="underline cursor-pointer">
+            {variant === "LOGIN" ? "Create New Account" : "Login"}
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ app/components/Button.tsx
@@ -0,0 +1,51 @@
+"use client";
+import clsx from "clsx";
+
+interface ButtonProps {
+  type?: "button" | "submit" | "reset" | undefined;
+  fullWidth?: boolean;
+  children: React.ReactNode;
+  onClick?: () => void;
+  secondary?: boolean;
+  danger?: boolean;
+  disabled?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = (
+  { type, fullWidth, children, onClick, secondary, danger, disabled },
+  ...props
+) => {
+  return (
+    <button
+      onClick={onClick}
+      type={type}
+      disabled={disabled}
+      className={clsx(
+        `
+    flex
+    justify-center
+    rounded-md
+    px-3
+    py-2
+    text-sm
+    font-semibold
+    focus-visible:outline
+    focus-visible:outline-2
+    focus-visible:outline-offset-2
+  `,
+        disabled && "opacity-50 cursor-default",
+        fullWidth && "w-full",
+        secondary ? "text-gray-900" : "text-white",
+        danger &&
+          "bg-rose-500 hover:bg-rose-600 focus-visible:outline-rose-600",
+        !secondary &&
+          !danger &&
+          "bg-indigo-600 hover:bg-indigo-700 focus-visible:outline-indigo-700"
+      )}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
